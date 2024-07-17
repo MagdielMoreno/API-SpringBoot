e359eb66a9dba2d53cc49be8aa8f401751e44148
“MagdielMoreno” <magma922002@gmail.com>
2024-07-16 23:07:10 -0700

    +

--- src/main/java/com/magdiel/api/models/Student.java
+++ src/main/java/com/magdiel/api/models/Student.java
@@ -3,7 +3,6 @@ package com.magdiel.api.models;
 import java.io.Serializable;
 import java.util.Objects;
 import java.util.List;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
