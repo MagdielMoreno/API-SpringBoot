7a5c33f631340ebad87b9799481aac50f156479c
MagdielMoreno <magma922002@gmail.com>
2024-07-17 11:45:47 -0700

    Cors Config

--- src/main/java/com/magdiel/api/ApiApplication.java
+++ src/main/java/com/magdiel/api/ApiApplication.java
@@ -1,7 +1,12 @@
 package com.magdiel.api;
 
+import java.util.Arrays;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 // Magdiel's Dev Api
 @SpringBootApplication
@@ -10,4 +15,18 @@ public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
+
+	@Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://mi-aplicacion-react.com"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
 }
